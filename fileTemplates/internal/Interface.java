46a2a0d8566cd944dbe4801abebcd85f037ed585
yuyue <yuyue@weshare.com.cn>
2018-11-21 17:37:27 +0800

    IU-2018.2.5 <yuyue@DESKTOP-QBN2KJV Create baseRefactoring.xml, markdown.xml, ide.general.xml, git.xml, CodeGlance.xml, project.default.xml, rainbow_brackets.xml, diff.xml, Default copy.xml, Interface.java, lombok-plugin.xml, other.xml, yiiguxing.translation.xml, Default.xml, applicationLibraries.xml, ignore.xml, find.xml, ui.lnf.xml, File Header.java, filetypes.xml, keymap.xml, Java.xml, plugin_ui.xml, colors.scheme.xml, html_xml.xml, databaseDrivers.xml, github_settings.xml, mavenVersion.xml, templates.xml, javarebel.xml, my.xml, SQL.xml, customization.xml, Default.xml, scala.xml, laf.xml, Class.java, jdk.table.xml, debugger.renderers.xml, path.macros.xml, web-browsers.xml, vcs.xml, editor.codeinsight.xml, grepConsole.xml, databaseSettings.xml, editor.xml, usageView.xml, debugger.xml

---
+++ fileTemplates/internal/Interface.java
@@ -0,0 +1,9 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+/**
+ * ${DESCRIPTION}
+ * @author ${USER}
+ * @version ${YEAR}-${MONTH}-${DAY} ${HOUR}:${MINUTE}
+ */
+public interface ${NAME} {
+    
+}
